cc483daaf0547796b31d51a083402734c509671a
glennM <glennmoonie001@gmail.com>
2021-04-26 01:15:09 -0400

    inplemented functions for Vehicle.h in Vehicle.ccp

--- GlennM Inheritance and Polymorphism/Vehicle.cpp
+++ GlennM Inheritance and Polymorphism/Vehicle.cpp
@@ -2,3 +2,31 @@
 //04/25/2021
 //Vehicle Class
 
+#include "Vehicle.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void Vehicle::setManufacturer(string maker) {
+	manufacturer = maker;
+}
+
+string Vehicle::getManufacturer() {
+	return manufacturer;
+}
+
+void Vehicle::setYearMade(string year) {
+	yearMade = year;
+}
+
+string Vehicle::getYearMade() {
+	return yearMade;
+}
+
+void Vehicle::displayInfo() {
+
+	cout << "Vehicle Information:\n";
+	cout << "Manufacturer: " << manufacturer << endl;
+	cout << "Yeaar Built: " << yearMade << endl;
+}
